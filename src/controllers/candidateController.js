c30b4dc46f36a896b5c87b2143dcd8680eb240c2
harshita795 <yadavharshita261303@gmail.com>
2026-04-21 15:47:29 +0530

    feat: store mapped candidates in database #7

--- src/controllers/candidateController.js
+++ src/controllers/candidateController.js
@@ -1,10 +1,14 @@
 const {getCandidates} = require("../clients/atsClient");
 const {mapCandidates} = require("../mappers/candidateMapper");
+const {saveCandidate} = require("../services/candidateService");
 
 async function fetchCandidates(req, res) {
 try {
   const atsData = await getCandidates(); 
   const mappedData = mapCandidates(atsData);
+
+  // Save each Candidate
+  mappedData.forEach(saveCandidate)
 
   return res.json(mappedData);
   } catch (error) {
