f03dd08d952e054fdf717cde48fc70181cfced5d
harshita795 <yadavharshita261303@gmail.com>
2026-04-24 20:48:27 +0530

    chore: register update candidate route #9

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {syncCandidates} = require("../controllers/candidate/syncCandidates");
 const {getCandidates} = require("../controllers/candidate/getCandidates");
+const { updateCandidate } = require("../services/candidateService");
 
 router.get("/external/candidates", syncCandidates);   // ATS -> DB
 router.get("/candidates", getCandidates);  // From DB
+router.patch("/candidates/:id", updateCandidate)
 
 module.exports = router;
 
