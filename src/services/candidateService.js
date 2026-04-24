14bbe2058cf958758af62b934de10fbade6e2720
harshita795 <yadavharshita261303@gmail.com>
2026-04-24 20:47:52 +0530

    feat: add update candidate logic in service layer #9

--- src/services/candidateService.js
+++ src/services/candidateService.js
@@ -1,18 +1,5 @@
 const db = require("../config/db");
 
-function saveCandidate(candidate) {
-  const query = `INSERT OR REPLACE INTO candidates (id, name, email, phone, source, createdAt) Values (?, ?, ?, ?, ?, ?)`;
-
-  db.run(query, [
-    candidate.id,
-    candidate.name,
-    candidate.email,
-    candidate.phone,
-    candidate.source,
-    candidate.createdAt,
-  ]);
-}
-
 function runQuery(query, params = []){
   return new Promise((resolve, reject) => {
     db.all(query, params, (err, rows) => {
@@ -22,12 +9,60 @@ function runQuery(query, params = []){
   })
 }
 
+function runCommand(query, params = []){
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      if (err) return reject(err);
+      resolve(this);
+    })
+  })
+}
+
+async function saveCandidate(candidate) {
+  const query = `INSERT OR REPLACE INTO candidates (id, name, email, phone, source, createdAt) Values (?, ?, ?, ?, ?, ?)`;
+
+  await runCommand(query, [
+    candidate.id,
+    candidate.name,
+    candidate.email,
+    candidate.phone,
+    candidate.source,
+    candidate.createdAt,
+  ]);
+}
+
 async function getAllCandidates() {
   const rows = await runQuery("SELECT * FROM candidates");
   return rows;
 }
 
+async function updateCandidate(id, updates) {
+  const fields = [];
+  const values = [];
+
+  for (const key in updates) {
+    fields.push(`${key} = ?`);
+    values.push(updates[key]);
+  }
+
+  if(fields.length === 0){
+    throw new Error("No fields to update");
+  }
+
+  const query = `
+  UPDATE candidates
+  SET ${fields.join(", ")}
+  WHERE id = ?
+  `;
+
+  const result = await runCommand(query, [...values, id]);
+
+  return {changes: result.changes};
+
+}
+
 module.exports = {
   saveCandidate,
   getAllCandidates,
+  updateCandidate
 };
